47e1642a00e80a7b8f30c5bd8041f0594daece49
bolathedev <godswillchibuzororie@gmail.com>
2022-04-14 10:40:57 +0100

    pushing to github after commit all sub modules

---
+++ push.sh
@@ -0,0 +1,15 @@
+cd "company"
+
+git commit -m "most recent commit"
+
+cd ..
+
+cd "shops"
+
+git commit -m "most recent commit"
+
+cd ..
+
+git add .
+git commit -m "pushing to github after commit all sub modules"
+git push origin master
